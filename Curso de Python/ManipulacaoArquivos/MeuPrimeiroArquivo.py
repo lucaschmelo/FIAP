ebefcbca7a125a5f6aa178b0af3486d5aedbfe33
Lucas Melo <lucaschavesdm@hotmail.com>
2022-05-18 16:37:33 -0300

    att

---
+++ Curso de Python/ManipulacaoArquivos/MeuPrimeiroArquivo.py
@@ -0,0 +1,3 @@
+with open ("primeiro_arquivo.txt", "a") as arquivo: #w = Escrever, r = Ler, a = Escrever e Ler (insere um novo no já existente), x = Exclusivo
+    
+    arquivo.write ("\nMeu terceiro arquivo!")
