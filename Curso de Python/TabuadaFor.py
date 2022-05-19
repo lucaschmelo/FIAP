5ad2137a43cd9c96271077dc81c6708efae65d32
Lucas Melo <lucaschavesdm@hotmail.com>
2022-05-18 21:16:21 -0300

    att

--- Curso de Python/TabuadaFor.py
+++ Curso de Python/TabuadaFor.py
@@ -1,4 +1,4 @@
-tabuada=int(input("Digite um número maior que 2 e menor que 1000 para exibir a tabuada: "))
+tabuada=int(input("Digite um número para exibir a tabuada: "))
 print("Tabuada do número", tabuada)
 for valor in range(0,11,1): #início, fim e incremento
     print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
--- Curso de Python/VolumeCilindro.py
+++ Curso de Python/VolumeCilindro.py
@@ -1,10 +1,10 @@
 import math
 resposta = "S"
-while resposta == "S":
+for resposta in range (1,6):
     aluno=(input("\nAluno: "))
     altura=(float(input("Altura do cilindro: ")))
     raio=(float(input("Raio do cilindro: ")))
     volume=math.pi*raio*raio*altura
     print("-----------------------------------------")
     print("Volume do cilindro: ", volume)
-    resposta = input("\nDigite \"S\" para continuar incluindo alunos ou \"ENTER\" para finalizar: ").upper()
+    resposta = input("\nAperte enter para continuar incluindo alunos (limite de 5 alunos) ").upper()
